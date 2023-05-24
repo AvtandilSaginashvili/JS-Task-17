57bd07a269659b2a4f8b987f611e0cabebe2df6a
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-05-24 15:38:59 +0400

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+function convertation() {
+
+    var c = document.getElementById("converter").value;
+
+        document.getElementById("results").innerHTML = (c * 9/5) + 32;
+
+        document.getElementById("myform").reset();
+}
